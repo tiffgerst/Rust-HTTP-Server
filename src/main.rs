e0f8e2fdd996ad4dc30bd343ff30a1334b2c07e1
tiffgerst <tiffany.gerstmeyr@web.de>
2022-12-04 14:03:17 +0000

    server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
 fn main() {
-    println!("Hello, world!");
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
+}
+
+struct Server{
+    addr:String,
+}
+
+impl Server {
+    fn new(addr: String) -> Self{
+        Self {
+            addr
+        }
+    }
+
+    fn run(self){
+        println!("Listening on {}", self.addr)
+    }
+}
+
+struct Request{
+    path: String,
+    query:Option<String>,
+    method: Method,
+}
+
+enum Method {
+    GET,
+    DELETE,
+    POST,
+    PUT,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH,
 }
